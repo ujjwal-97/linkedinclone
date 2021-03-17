853ee1a78b8624578522f0d9b12b0e06b7a8dc00
ujjwal <ujjwal97@hotmail.com>
2021-03-17 23:53:09 +0530

    new post input box added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Feed from './Feed';
 import './App.css';
 
 export default function App() {
@@ -11,7 +12,7 @@ export default function App() {
       {/* App Body */}
       <div className="app__body">
         <Sidebar />
-        {/* Feed */}
+        <Feed />
         {/* Widgets */}
       </div>
     </div>
