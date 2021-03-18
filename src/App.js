1bc10418d658fc1b2361be1ddafe0840468b9def
ujjwal <ujjwal97@hotmail.com>
2021-03-19 00:01:50 +0530

    Integrated Redux

--- src/App.js
+++ src/App.js
@@ -7,9 +7,7 @@ import './App.css';
 export default function App() {
   return (
     <div className="app">
-      {/* Header */}
       <Header />
-      {/* App Body */}
       <div className="app__body">
         <Sidebar />
         <Feed />
